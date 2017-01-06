22617bcc91cb4f264c955ad1a3c1a785253e8735
daimashen <13464735046@163.com>
2017-01-06 10:43:37 +0800

    demo
    实时通信小样

---
+++ demo/js/server.js
@@ -0,0 +1,23 @@
+var fs = require('fs'),
+    http = require('http'),
+    sio = require('socket.io');
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.end(fs.readFileSync('./index.html'));
+});
+server.listen(8888, function() {
+    console.log('Server listening at http://localhost:8888/');
+});
+io = sio.listen(server);
+var messages = [];
+io.sockets.on('connection', function(socket) {
+    socket.on('message', function(msg) {
+        console.log('Received: ', msg);
+        messages.push(msg);
+        socket.broadcast.emit('message', msg);
+    });
+    messages.forEach(function(msg) {
+        socket.send(msg);
+    })
+});
